83958747bcfdcd6678a5b341c59f66adf4847684
Rika <tianlixiang35@gmail.com>
2026-07-28 11:19:04 +0900

    SECRET_KEYを環境変数から読み込むように変更

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^l!(53@+c!t8^ot9&y#m87+zxc($(da%zyw+ry)63a22n@*z0!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
