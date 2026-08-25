0b2e8f478208832bb7f6760fae7504c0dfe936f1
Rika <tianlixiang35@gmail.com>
2026-08-25 17:23:59 +0900

    django-storages導入・ローカル動作確認・テストデータ削除

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +25,7 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('pets/', include('pets.urls')),
 ]
+
+# ローカル開発時にメディアファイル（ペットアイコン）を提供する
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
