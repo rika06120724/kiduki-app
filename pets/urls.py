c689acc73048350e775ef1f6406bdee977bed4a5
Rika <tianlixiang35@gmail.com>
2026-08-28 17:23:49 +0900

    記録フォーム実装（複数ステップ対応・表示調整完了）

--- pets/urls.py
+++ pets/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import create_pet, my_page, pet_edit
+from .views import create_pet, my_page
 
 app_name = 'pets'
 urlpatterns = [
     path('register/', create_pet, name='register'),
     path('my_page/', my_page, name='my_page'),
-    path('<int:pet_id>/edit/', pet_edit, name='edit'),  # ペット編集ビューへのURLパターンを追加
 ]
